7698619704d4d1410f7b57040267d67f81c646ec
AndreBauzil <89871705+AndreBauzil@users.noreply.github.com>
2023-09-26 22:43:17 +0000

    Lidando com diversas promises e multi-requisições, além de criação de método separado para pegar detalhes de cada pokemon

--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -1,11 +1,26 @@
 
-
 const pokeAPI = {}
 
-pokeAPI.getPokemons = (offset = 0, limit = 10) => {
+pokeAPI.getPokemonDetail = (pokemon) => {
+    return fetch(pokemon.url).then((response) => response.json())
+}
+
+pokeAPI.getPokemons = (offset = 0, limit = 5) => {
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
     return fetch(url)
         .then((response) => response.json())
         .then((jsonBody) => jsonBody.results)
-        .catch((error) => console.error(error))
+        .then((pokemons) => pokemons.map(pokeAPI.getPokemonDetail))
+        .then((detailRequest) => Promise.all(detailRequest))
+        .then((pokemonsDetail) => pokemonsDetail)
 }
+
+// Promise.all([
+//     fetch('https://pokeapi.co/api/v2/pokemon/1'),
+//     fetch('https://pokeapi.co/api/v2/pokemon/2'),
+//     fetch('https://pokeapi.co/api/v2/pokemon/3'),
+//     fetch('https://pokeapi.co/api/v2/pokemon/4')
+// ]).then((results) => {
+// console.log(results)
+// })
