eb67939ffde62e0aa6e7b08f68197a117ddfee27
Andre Bauzil <andregbauzil@gmail.com>
2023-10-20 21:14:11 -0300

    Adição de import (request) para todas as classes execução para teste, além de enunciado da função para melhor entendiment

---
+++ challenges/first/index.js
@@ -0,0 +1,14 @@
+console.log("Função - Equilibrando Saldo");
+const eqSaldo = require('./EquilibrarSaldo');
+
+console.log("\nFunção - Organizando Ativos")
+const orgAtivos = require('./OrganizandoAtivos');
+
+console.log("\nFunção - Realizar Saque")
+const realiSaq = require('./RealizarSaque');
+
+console.log("\nFunção - Juros Compostos")
+const jurosComp = require('./JurosCompostos');
+
+console.log("\nFunção - Teste de Depósito")
+const testDepo = require('./TesteDeposito');
